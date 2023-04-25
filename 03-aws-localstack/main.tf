d8f17031e466afc2ca4df55b116918d52f8e3554
Alexandr Kastritski <54674927+AlexKastrytski@users.noreply.github.com>
2023-04-25 16:07:29 +0300

    Update main.tf

--- 03-aws-localstack/main.tf
+++ 03-aws-localstack/main.tf
@@ -13,20 +13,20 @@ provider "aws" {
   }
 }
 
-terraform {
-  backend "s3" {
-    bucket                      = "tf-state"
-    key                         = "terraform.tfstate"
-    region                      = "us-east-1"
-    endpoint                    = "http://s3.localhost.localstack.cloud:4566"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    force_path_style            = true
-    dynamodb_table              = "terraform-lock"
-    dynamodb_endpoint           = "http://localhost:4566"
-    encrypt                     = true
-  }
-}
+#terraform {
+ # backend "s3" {
+ #   bucket                      = "tf-state"
+ #   key                         = "terraform.tfstate"
+ #  region                      = "us-east-1"
+ #  endpoint                    = "http://s3.localhost.localstack.cloud:4566"
+ #  skip_credentials_validation = true
+ #  skip_metadata_api_check     = true
+ #  force_path_style            = true
+ #   dynamodb_table              = "terraform-lock"
+ #   dynamodb_endpoint           = "http://localhost:4566"
+ #  encrypt                     = true
+ # }
+#}
 
 resource "aws_s3_bucket" "tfstate_bucket" {
   bucket = "tf-state"
